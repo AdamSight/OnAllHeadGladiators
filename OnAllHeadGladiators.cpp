dc3a0c5e5a7ec70c1803f8b3c72eac2ebc971ae1
AdamSight <34420437+AdamSight@users.noreply.github.com>
2017-12-10 18:57:04 +0300

    maingame.cpp

---
+++ OnAllHeadGladiators.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+//тут ещё короче надо подключить dos.h , что-бы функцию delay() можно было вызывать. Но у меня не получилось
+
+
+using namespace std;
+
+string usrlearn();
+void nainm();
+
+int main(int argc, const char * argv[]){
+
+string name;
+
+usrlearn();
+mainm();
+
+}
+
+//обучение
+string usrlearn(){
+  std::cout << "Привет! Ну как, оЧУХАлся? " << '\n';
+  std::cout << "Ну, тут вообщем дело такое. Ты теперь - гладиатор !" << '\n';
+  std::cout << "Да, да. Всё вы тут 'да я просто домой шёл, как друг темнота...' " << '\n';
+  std::cout << "Так что, оружее в руки, и бегом переобучаться, хех. " << '\n';
+  std::cout << "Вот, кстати, наш арсенал для новичков, выбирай с умом. " << '\n';
+
+}
+
+//главное меню
+void mainm(){
+
+}
