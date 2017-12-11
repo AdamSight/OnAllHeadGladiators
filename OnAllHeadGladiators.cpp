3ac5c4fe1b4b6a0bba71ea806297d8780d5527ad
AdamSight <34420437+AdamSight@users.noreply.github.com>
2017-12-11 13:27:28 +0300

    Add files via upload

--- OnAllHeadGladiators.cpp
+++ OnAllHeadGladiators.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 string usrlearn();
-void nainm();
+void mainm();
 
 int main(int argc, const char * argv[]){
 
@@ -25,10 +25,30 @@ string usrlearn(){
   std::cout << "Да, да. Всё вы тут 'да я просто домой шёл, как друг темнота...' " << '\n';
   std::cout << "Так что, оружее в руки, и бегом переобучаться, хех. " << '\n';
   std::cout << "Вот, кстати, наш арсенал для новичков, выбирай с умом. " << '\n';
+  //todo запилить обучение
 
 }
 
 //главное меню
 void mainm(){
+  int mma;
 
-}
+
+  std::cout << "1.Персонаж" << '\n';
+  std::cout << "2.Магазин" << '\n';
+  std::cout << "3.Битва" << '\n';
+  std::cin >> mma;
+
+do {
+  bool flag = true;
+
+  if (mma == 1){
+   user();
+ } else if (mma == 2) {
+   shop();
+ } else if (mma == 3) {
+   fightpick();
+ } else {
+   flag = false;
+ }
+} while(!flag);
